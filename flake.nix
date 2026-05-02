ed3d2571548eb463cc71ba74c8d6ccdecdbb4667
myul <52401682+mtul0729@users.noreply.github.com>
2026-05-02 18:17:19 +0800

    Use GitHub flake URL for Steel

--- flake.nix
+++ flake.nix
@@ -5,7 +5,7 @@
     flake-parts.url = "github:hercules-ci/flake-parts";
     nixpkgs.url = "github:nixos/nixpkgs/nixos-unstable";
     rust-overlay.url = "github:oxalica/rust-overlay";
-    steel.url = "git+https://github.com/mattwparas/steel.git";
+    steel.url = "github:mattwparas/steel";
     systems.url = "github:nix-systems/default";
   };
 
